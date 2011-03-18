1ac919deb62ef6b86c31827291337606efeb02bc
joeljunstrom <joel@winstondesign.se>
2011-03-18 12:25:36 +0100

    Y U NO ACCEPT NORMAL ROUTES URL RESOLVER?

--- lib/porthos/url_resolver.rb
+++ lib/porthos/url_resolver.rb
@@ -2,8 +2,8 @@ require 'routing_filter'
 module RoutingFilter
   class UrlResolver < Filter
     def around_recognize(path, env, &block)
-      if node = Node.find_by_url(path)
-        yield.tap do |params|
+      yield.tap do |params|
+        if node = Node.find_by_url(path)
           params[:controller] = node.controller
           params[:action]     = node.action
           params[:id]         = node.resource_id if node.resource_id.present?

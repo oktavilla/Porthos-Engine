00a01e77de1c75a86402244a42212d5bf4afc7f0
arvida <arvid@winstondesign.se>
2011-03-22 18:13:44 +0100

    Moved error message to i18n files

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,12 +1,14 @@
 class Redirect < ActiveRecord::Base
   validates_presence_of :path, :target
   validates_length_of :path, :minimum => 2
-  validates_uniqueness_of :path 
-  
-  validates_format_of :path, :with => /^\/(.*?)/, :message => 'måste börja med ett /-tecken'
-  
+  validates_uniqueness_of :path
+
+  validates_format_of :path,
+                      :with => /^\/(.*?)/,
+                      :message => I18n.t(:bad_redirect_format, :scope => [:app, :validators])
+
   before_save :remove_trailing_slash
-  
+
 protected
   def remove_trailing_slash
     path.chop! if path[-1,1] == '/'

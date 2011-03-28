e78b2ab23d7acc79a48526ccd288bb3a40709973
Joel Junström <joel@Joel-Junstroms-iMac-4.local>
2011-03-28 16:20:19 +0200

    Astrated on acts as taggable test, needs test for namespaced tags and finders

---
+++ test/unit/acts_as_taggable_test.rb
@@ -0,0 +1,25 @@
+require_relative '../test_helper'
+
+class ActsAsTaggableTest < ActiveSupport::TestCase
+  context 'A model with acts as taggable' do
+    setup do
+      @page = Factory(:page)
+      @tag_names = %w(tag1 tag2 tag3)
+      @page.tag_names = @tag_names.join(Tag.delimiter)
+    end
+    subject { @page }
+
+    should have_many(:taggings).dependent(:destroy)
+    should have_many(:tags).through(:taggings)
+    should have_many(:all_tags).through(:taggings)
+
+    should 'have created tags from the tag_names string' do
+      assert_equal @tag_names, @page.tags.collect(&:name)
+    end
+
+    should 'make the tags accesible as a string' do
+      assert_equal @tag_names.join(Tag.delimiter), @page.tag_names
+    end
+
+  end
+end

11b33976bfce145c283cbb7e4a4854652f02b14b
arvida <arvid@winstondesign.se>
2011-04-07 13:10:47 +0200

    Commentet out acts_as_list for asset usage

--- app/models/asset_usage.rb
+++ app/models/asset_usage.rb
@@ -2,11 +2,11 @@ class AssetUsage < ActiveRecord::Base
   belongs_to :parent, :polymorphic => true
   belongs_to :asset
 
-  acts_as_list :scope => 'parent_id = #{parent_id} and parent_type = \'#{parent_type}\'', :column => 'position', :order => 'position'
+  # acts_as_list :scope => 'parent_id = #{parent_id} and parent_type = \'#{parent_type}\'', :column => 'position', :order => 'position'
 
   # Used when uploading new images to a product
   attr_accessor :file
-  
+
   validates_presence_of :parent_id, :parent_type
   validates_presence_of :asset_id, :if => Proc.new { |a| !(a.file and a.file.size.nonzero?) }
   before_save :store_asset

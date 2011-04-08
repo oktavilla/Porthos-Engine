614757a750b57a962d7efd632e38e04922767f1e
Alexis Fellenius <alexis@winstondesign.se>
2011-04-08 17:35:52 +0200

    Adjustments to CKeditor

--- public/javascripts/admin/ckeditor/config.js
+++ public/javascripts/admin/ckeditor/config.js
@@ -2,18 +2,25 @@
 Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
+CKEDITOR.stylesSet.add( 'my_styles',
+[
+    // Block-level styles
+    { name : 'Lead', element : 'p', attributes : { 'class' : 'lead' } },
+]);
 
 CKEDITOR.editorConfig = function(config) {
   config.language = 'en';
   config.entities = false;
   config.toolbar_Basic = [
-    ['Format'],
-    ['NumberedList','BulletedList','Outdent','Indent'],
-    ['Blockquote','CreateDiv'],
+    ['Format','Styles'],
+    ['Bold','Italic','Strike'],
+    ['NumberedList','BulletedList'],
+    ['Blockquote'],
     ['Link','Unlink','Anchor'],
     ['PasteFromWord'],
     ['Table'],
     ['Maximize', 'ShowBlocks', 'Source']
   ];
+  config.stylesSet = 'my_styles';
   config.toolbar = 'Basic';
 };

0f987fd084eca6959df470aff7ecf54e767e16a6
Joel Junström <joel.junstrom@oktavilla.se>
2011-04-27 11:52:59 +0200

    Fixed faulty within checks for assets test

--- test/integration/admin/assets_test.rb
+++ test/integration/admin/assets_test.rb
@@ -13,7 +13,7 @@ class AssetsTest < ActiveSupport::IntegrationCase
     asset = Factory.create(:asset, :file => new_tempfile('text'))
     visit admin_assets_path
 
-    assert_match asset.name, page.body
+    assert page.find("ul.items #asset_#{asset.id}").has_content?(asset.name), 'Should display the asset in the assets list'
   end
 
   test 'uploading a new asset' do
@@ -22,21 +22,21 @@ class AssetsTest < ActiveSupport::IntegrationCase
 
     click_link I18n.t(:'app.views.assets.index.upload_files')
 
-    assert_equal '/admin/assets/new', current_path
+    assert_equal new_admin_asset_path, current_path
 
     attach_file 'asset_file', test_file_path('page.txt')
     click_button I18n.t(:'app.views.assets.new.submit')
 
-    assert_equal '/admin/assets/incomplete', current_path
+    assert_equal incomplete_admin_assets_path, current_path
 
     fill_in 'asset_0_title', :with => 'My file'
     fill_in 'asset_0_description', :with => 'A file about stuff'
     fill_in 'asset_0_author', :with => 'A. User'
     click_button I18n.t(:save)
 
-    assert_equal '/admin/assets', current_path
-    assert_match 'My file', page.body
-    assert_match 'A file about stuff', page.body
+    assert_equal admin_assets_path, current_path
+    assert page.find("ul.items").has_content?('My file')
+    assert page.find('ul.items').has_content?('A file about stuff')
   end
 
   test 'editing a asset' do
@@ -50,10 +50,8 @@ class AssetsTest < ActiveSupport::IntegrationCase
     fill_in 'asset_title', :with => 'My updated file'
     click_button I18n.t(:save)
 
-    assert_equal '/admin/assets', current_path
-    within("#asset_#{asset.id}") do
-      assert_match 'My updated file', page.body
-    end
+    assert_equal admin_assets_path, current_path
+    assert page.find("#asset_#{asset.id}").has_content?('My updated file'), 'Should show the new asset name'
   end
 
   test 'deleting an asset' do
@@ -66,8 +64,8 @@ class AssetsTest < ActiveSupport::IntegrationCase
       click_link I18n.t(:destroy)
     end
 
-    assert_equal '/admin/assets', current_path
-    assert !page.body.include?("asset_#{asset.id}"), 'Asset removed from page body'
+    assert_equal admin_assets_path, current_path
+    assert page.has_no_css?("asset_#{asset.id}"), 'Asset removed from page body'
     assert Asset.count.zero?, 'Asset deleted from db'
   end
 end

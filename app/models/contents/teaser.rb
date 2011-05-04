d09255ff5070fc4e45bd13516e493bff5f5219a8
Joel Junström <joel.junstrom@oktavilla.se>
2011-05-04 18:28:33 +0200

    Fixed adding assets to teasers

--- app/models/contents/teaser.rb
+++ app/models/contents/teaser.rb
@@ -1,3 +1,13 @@
 class Teaser < Textfield
-  has_many :assets
+  key :asset_ids, Array, :typecast => 'ObjectId'
+  has_many :assets, :in => :asset_ids
+
+  def new_asset_id=(asset_id)
+    self.asset_ids << asset_id unless asset_ids.one? { |i| i.to_s == asset_id }
+  end
+
+  def remove_asset=(asset_id)
+    self.asset_ids.delete_if { |i| i.to_s == asset_id }
+  end
+
 end

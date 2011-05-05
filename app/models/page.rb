ea7d3af72605ccfdb0b7a835a9c389987f4acfab
Arvid Andersson <arvid@winstondesign.se>
2011-05-05 17:43:42 +0200

    Reenabled categories on pages, still a few bugs

--- app/models/page.rb
+++ app/models/page.rb
@@ -186,7 +186,7 @@ class Page
   end
 
   def category
-    #@category ||= field_set.allow_categories? ? all_tags.with_namespace(field_set.handle).first : nil
+    @category ||= field_set.allow_categories? ? Page.tags_by_count(:namespace => field_set.handle).first : nil
   end
 
   def category_name

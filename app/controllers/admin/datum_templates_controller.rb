aecafa9cd71ba1b487a945a0bf3cf251af19c154
Joel Junström <joel.junstrom@oktavilla.se>
2011-05-16 21:36:49 +0200

    Fixed redirects for datum templates

--- app/controllers/admin/datum_templates_controller.rb
+++ app/controllers/admin/datum_templates_controller.rb
@@ -13,7 +13,7 @@ class Admin::DatumTemplatesController < ApplicationController
     if @datum_template.save
       flash[:notice] = "#{@datum_template.label}  #{t(:saved, :scope => [:app, :admin_general])}"
     end
-    respond_with @datum_template, :location => admin_page_template_path(@template)
+    respond_with @datum_template, :location => url_for(:controller => @template.class.to_s.tableize, :action => 'show', :id => @template.id.to_s)
   end
 
   def edit
@@ -25,7 +25,7 @@ class Admin::DatumTemplatesController < ApplicationController
     if @datum_template.update_attributes(params[:datum_template])
       flash[:notice] = "#{@datum_template.label} #{t(:saved, :scope => [:app, :admin_general])}"
     end
-    respond_with @datum_template, :location => admin_page_template_path(@template)
+    respond_with @datum_template, :location => url_for(:controller => @template.class.to_s.tableize, :action => 'show', :id => @template.id.to_s)
   end
 
   def destroy
@@ -33,12 +33,17 @@ class Admin::DatumTemplatesController < ApplicationController
     if @template.pull(:datum_templates => { :_id => @datum_template.id })
       flash[:notice] = "#{@datum_template.label}  #{t(:deleted, :scope => [:app, :admin_general])}"
     end
-    respond_with @datum_template, :location => admin_page_template_path(@template)
+    respond_with @datum_template, :location => url_for(:controller => @template.class.to_s.tableize, :action => 'show', :id => @template.id.to_s)
   end
 
   def sort
-    params[:datum_template].each_with_index do |id, i|
-      Datumtemplate.update_all({:first => (i == 0), :next_id => params[:datum_template][i+1]}, ["id = ?", id])
+    if params[:datum_template]
+      params[:datum_template].each_with_index do |id, i|
+        @template.datum_templates.detect { |c| c.id.to_s == id }.tap do |datum_template|
+          datum_template.position = i
+        end
+      end
+      @template.save
     end
     respond_to do |format|
       format.js { render :nothing => true }

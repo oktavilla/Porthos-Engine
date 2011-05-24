aa2c1665a79a37548cdfe02eae4a823767a459fb
Joel Junström <joel.junstrom@oktavilla.se>
2011-05-24 15:45:32 +0200

    FieldSet#data_attributes= now takes either an hash or array with attributes

--- test/unit/field_set_test.rb
+++ test/unit/field_set_test.rb
@@ -2,30 +2,54 @@ require_relative '../test_helper'
 
 class FieldSetTest < ActiveSupport::TestCase
 
-  test 'creating a field set from a field set template' do
-    field_set_template = Factory.build(:field_set_template)
-    field_set = FieldSet.from_template(field_set_template)
-    assert_equal field_set_template.label, field_set.label, "Should have cloned the fields's label"
-    assert_equal field_set_template.handle, field_set.handle, "Should have cloned the fields's handle"
-    assert_equal field_set_template.required?, field_set.required?, "Should have cloned the fields's required?"
+  setup do
+    @field_set_template = Factory.build(:field_set_template)
+    @field_set = FieldSet.from_template(@field_set_template)
+  end
 
-    field_set_template.content_template.datum_templates.each do |field_template|
-      field_set.data[field_template.handle].tap do |datum|
+  test 'sanity' do
+    assert @field_set_template.content_template.datum_templates.any?
+  end
+
+  test 'mirrored the field_templates attributes' do
+    assert_equal @field_set_template.label, @field_set.label, "Should have cloned the fields's label"
+    assert_equal @field_set_template.handle, @field_set.handle, "Should have cloned the fields's handle"
+    assert_equal @field_set_template.required?, @field_set.required?, "Should have cloned the fields's required?"
+  end
+
+  test 'created data from the datum templates' do
+    @field_set_template.content_template.datum_templates.each do |field_template|
+      @field_set.data[field_template.handle].tap do |datum|
         assert datum.kind_of?(Field), "Should have created fields from the field templates"
         assert_equal field_template.shared_attributes, datum.attributes.except(:_id, :_type, :value, :active), "Should have mirrored the field_template attributes to the field"
       end
     end
   end
 
-  test 'has many data' do
-    assert FieldSet.new.respond_to?(:data)
+  test 'finding data by their handle' do
+    @field_set.data.each do |field|
+      assert_equal field, @field_set.data[field.handle], "Should have found a field by it's handle"
+    end
   end
 
-  test 'finding fields by their handle' do
-    field_set = Factory.build(:field_set)
-    field_set.data.each do |field|
-      assert_equal field, field_set.data[field.handle], "Should have found a field by it's handle"
-    end
+  test 'updating data with an hash of attributes' do
+    @field_set.data_attributes = {
+      '0' => {
+        id: @field_set.data.first.id,
+        value: 'The truth may be out there, but the lies are inside your head.'
+      }
+    }
+
+    assert_equal 'The truth may be out there, but the lies are inside your head.', @field_set.data.first.value, 'Should have set the value via an hash'
+  end
+
+  test 'updating data with an array of attributes' do
+    @field_set.data_attributes = [{
+      id: @field_set.data.first.id,
+      value: 'If you put butter and salt on it, it tastes like salty butter.'
+    }]
+
+    assert_equal 'If you put butter and salt on it, it tastes like salty butter.', @field_set.data.first.value, 'Should have set the value via an hash'
   end
 
 end

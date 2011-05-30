e21e80027352aba64ef8d478271e93a8d8884222
Arvid Andersson <arvid@winstondesign.se>
2011-05-30 18:18:08 +0200

    Tmp disable of index setting

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -8,10 +8,10 @@ MongoMapper.connect(Rails.env)
 MongoMapper.database = "porthos-#{Rails.env}"
 
 
-Asset.ensure_index :created_at
-Asset.ensure_index :updated_at
+#Asset.ensure_index :created_at
+#Asset.ensure_index :updated_at
 
-Page.ensure_index :page_template_id
-Page.ensure_index :updated_by_id
-Page.ensure_index :created_at
-Page.ensure_index :updated_at
+#Page.ensure_index :page_template_id
+#Page.ensure_index :updated_by_id
+#Page.ensure_index :created_at
+#Page.ensure_index :updated_at

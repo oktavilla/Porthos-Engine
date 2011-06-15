f56c6799cd39609e53afa1c4442ac53fbf0a664a
Joel Junström <joel.junstrom@oktavilla.se>
2011-06-15 15:01:23 +0200

    Fixed datum uniqueness of handle test

--- test/unit/datum_test.rb
+++ test/unit/datum_test.rb
@@ -4,15 +4,15 @@ class DatumTest < ActiveSupport::TestCase
 
   test 'uniqueness of handle within collection' do
     parent = Factory.build(:page, :data => [])
-    Factory.build(:field, :handle => 'le_handle').tap do |datum|
+    Factory.build(:string_field, :handle => 'le_handle').tap do |datum|
       parent.data << datum
-      assert datum.valid?
+      assert datum.valid?, "should be valid"
     end
 
-    Factory.build(:field, :handle => 'le_handle').tap do |datum|
+    Factory.build(:date_field, :handle => 'le_handle').tap do |datum|
       parent.data << datum
-      assert !datum.valid?
-      assert_not_nil datum.errors.on(:handle)
+      assert !datum.valid?, "should not be valid"
+      assert_not_nil datum.errors[:handle], "should have errors on handle"
     end
   end
 

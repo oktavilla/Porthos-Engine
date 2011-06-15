c974d800b0b354ed41dd0e838b43c93e135cae70
Joel Junström <joel.junstrom@oktavilla.se>
2011-06-15 13:01:30 +0200

    Fixed datum unit test

--- test/unit/datum_test.rb
+++ test/unit/datum_test.rb
@@ -5,12 +5,12 @@ class DatumTest < ActiveSupport::TestCase
   test 'uniqueness of handle within collection' do
     parent = Factory.build(:page, :data => [])
     Factory.build(:field, :handle => 'le_handle').tap do |datum|
-      parent.fields << datum
+      parent.data << datum
       assert datum.valid?
     end
 
     Factory.build(:field, :handle => 'le_handle').tap do |datum|
-      parent.fields << datum
+      parent.data << datum
       assert !datum.valid?
       assert_not_nil datum.errors.on(:handle)
     end

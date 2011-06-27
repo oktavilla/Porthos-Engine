3b89fd0ae2280340d81d559d37a54c77e11708d3
Joel Junström <joel.junstrom@oktavilla.se>
2011-06-27 19:09:06 +0200

    Propagate removal of content templates

--- app/models/templates/content_template.rb
+++ app/models/templates/content_template.rb
@@ -1,4 +1,6 @@
 class ContentTemplate < Template
+  after_destroy :propagate_removal_to_pages
+
   def to_datum
     FieldSet.new.tap do |field_set|
       field_set.attributes = self.shared_attributes
@@ -7,6 +9,28 @@ class ContentTemplate < Template
   end
 
   def shared_attributes
-    { :label => self.label }
+    { :label => self.label, :content_template_id => self.id }
   end
+
+private
+
+  # TODO: Add delayed job
+  def propagate_removal_to_pages
+    # Pull all field sets in pages connected to this
+    Page.pull({}, data: { 'content_template_id' => self.id })
+
+    # Pull all field sets in content blocks connected to this
+    Page.collection.update({
+      'data' => {
+        '$elemMatch' => { 'data.content_template_id' => self.id }
+      }
+    }, {
+      '$pull' => {
+        'data.$.data' => { 'content_template_id' => self.id }
+      }
+    })
+
+    PageTemplate.pull({}, datum_templates: { 'content_template_id' => self.id })
+  end
+
 end

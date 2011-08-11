6b276d0177d8c296f22dae9e929ece94751359b8
Joel Junström <joel.junstrom@oktavilla.se>
2011-08-11 16:38:35 +0200

    Allow self.page_id as a possible target

--- app/models/data/page_association.rb
+++ app/models/data/page_association.rb
@@ -16,7 +16,7 @@ class PageAssociation < Datum
     return @targets if @targets
     exclude_ids = [self._root_document.id]
     if _parent_document.is_a?(DatumCollection)
-      exclude_ids += _parent_document.data.find_all { |d| d.is_a?(PageAssociation) }.map { |d| d.page_id }
+      exclude_ids += _parent_document.data.find_all { |d| d != self && d.is_a?(PageAssociation) }.map { |d| d.page_id }
     end
     scope = Page.fields(:_id, :title).where(:_id.nin => exclude_ids)
     scope = scope.where(:page_template_id.in => page_template_ids) if page_template_ids && page_template_ids.any?

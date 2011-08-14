ff559f2db93584eea1e5a1b7eddf6c15b1152e3c
Joel Junström <joel.junstrom@oktavilla.se>
2011-08-14 21:25:16 +0200

    Check for Item and not Page in the link_path helper. Closes #81

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -3,7 +3,7 @@ module DataHelper
     if link.url.present?
       link.url
     elsif link.resource
-      link.resource.is_a?(Page) ? page_path(link.resource) : link.resource.url
+      link.resource.kind_of?(Item) ? page_path(link.resource) : link.resource.url
     end
   end
 end

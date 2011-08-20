84275a04ec9f055da7ce5835a102bedc2d8b6c15
Joel Junström <joel.junstrom@oktavilla.se>
2011-08-20 18:36:42 +0200

    Small refactor of link_path helper, might make it more stable. or not

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -1,9 +1,9 @@
 module DataHelper
   def link_path(link)
-    if link.url.present?
+    if link['url'].present?
       link.url
     elsif link.resource
-      link.resource.kind_of?(Item) ? page_path(link.resource) : link.resource.url
+      link.resource['url'] ? link.resource.url : page_path(link.resource)
     end
   end
 end

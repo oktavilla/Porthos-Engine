aa7cb4a70fe9ce60bd140108bd4f603d6233fefc
Joel Junström <joel.junstrom@oktavilla.se>
2011-09-01 11:11:16 +0200

    Allow link argument to be nil

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -1,9 +1,11 @@
 module DataHelper
   def link_path(link)
-    if link['url'].present?
-      link.url
-    elsif link.resource
-      link.resource['url'] ? link.resource.url : page_path(link.resource)
+    if link
+      if link['url'].present?
+        link.url
+      elsif link.resource
+        link.resource['url'] ? link.resource.url : page_path(link.resource)
+      end
     end
   end
 end

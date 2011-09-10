517ba47aa93fcbd88702e7b924910f08139b1eeb
Joel Junström <joel.junstrom@oktavilla.se>
2011-09-10 12:37:45 +0200

    Fixed generator

--- lib/generators/porthos/porthos_generator.rb
+++ lib/generators/porthos/porthos_generator.rb
@@ -1,24 +1,35 @@
-require 'rails/generators/migration'
-
 class PorthosGenerator < Rails::Generators::Base
-  include Rails::Generators::Migration
+  source_root File.expand_path("../templates", __FILE__)
 
-  def self.source_root
-    @source_root ||= File.expand_path('../templates', __FILE__)
+  def application_name
+    Rails.application.class.name.split('::').first
   end
 
-  def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
-      Time.new.utc.strftime("%Y%m%d%H%M%S")
-    else
-      "%.3d" % (current_migration_number(dirname) + 1)
-    end
+  def create_initializer_file
+    initializer 'porthos.rb', <<-FILE
+Porthos.s3_storage = Porthos::S3Storage.new({
+  :bucket_name => "porthos-assets",
+  :access_key_id => '',
+  :secret_access_key => ''
+})
+
+Porthos.configure do |config|
+  config.observers = :node_observer
+
+  config.resizor do |resizor|
+    resizor.api_key = ''
   end
 
-  def create_migration_file
-    migration_template 'porthos_migration.rb', 'db/migrate/create_porthos_tables.rb'
-    copy_file 'seeds.rb', 'db/porthos_seeds.rb'
-    copy_file 'porthos.rb', 'config/initializers/porthos.rb'
+  config.tanking do |tanking|
+    tanking.index_name = "#{Rails.env}_#{application_name.underscore}".downcase
+    tanking.private_url = ''
+  end
+end
+#{application_name}::Application.config.index_tank_url = Porthos.config.tanking.private_url
+    FILE
+  end
+
+  def create_config_file
     copy_file 'mongo.yml', 'config/mongo.yml'
   end
 end

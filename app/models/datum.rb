439245d8f29ccb9b43bd2a6bdebfbaeadf1767a3
Joel Junström <joel.junstrom@oktavilla.se>
2011-09-16 14:28:05 +0200

    Datum now have a timestamp which gtes updated when saving a datum through data#update. Closes #122

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,8 +1,12 @@
 class Datum
   include MongoMapper::EmbeddedDocument
   include Porthos::DatumMethods
+
+  key :updated_at, Time
   key :datum_template_id, ObjectId
   key :active, Boolean, :default => lambda { true }
+
+  before_save :set_timestamps
 
   def root_embedded_document
     @root_embedded_document ||= _parent_document == _root_document ? self : _parent_document.try(:root_embedded_document)
@@ -12,6 +16,11 @@ class Datum
     @is_root ||= self == root_embedded_document
   end
 
+  def update_attributes(*args)
+    update_timestamps
+    super(*args)
+  end
+
   class << self
     def from_template(template)
       template.datum_class.constantize.new.tap do |field|
@@ -19,4 +28,15 @@ class Datum
       end
     end
   end
+
+private
+
+  def set_timestamps
+    update_timestamps if self.updated_at.nil?
+  end
+
+  def update_timestamps
+    self.updated_at = Time.now.utc
+  end
+
 end

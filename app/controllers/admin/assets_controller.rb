0d7e8d93e8a2fd86995e2d931e9323d9093db7ed
Arvid Andersson <arvid@winstondesign.se>
2011-10-26 12:06:06 +0200

    Removed compact from assets search result

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -33,7 +33,7 @@ class Admin::AssetsController < ApplicationController
       @query = params[:query]
       page = params[:page] || 1
       per_page = params[:per_page] ? params[:per_page].to_i : 45
-      @assets = Asset.search_tank(@query, :conditions => {'hidden' => false}, :per_page => per_page, :page => page).compact
+      @assets = Asset.search_tank(@query, :conditions => {'hidden' => false}, :per_page => per_page, :page => page)
       respond_to do |format|
         format.html do
           @current_tags = params[:tags] || []

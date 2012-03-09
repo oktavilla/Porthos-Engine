b296ae3b31b20d3198ceca50456a7eaaf8ff8bc5
Joel Junström <joel.junstrom@oktavilla.se>
2012-03-09 11:34:35 +0100

    Do not run find routes again if we do not find an route

--- lib/porthos/routing/inject_find_routes_filter.rb
+++ lib/porthos/routing/inject_find_routes_filter.rb
@@ -5,16 +5,15 @@ Journey::Router.class_eval do
 
     if matches.empty?
       excluded_path_prefixes = /(assets|admin|javascripts|stylesheets|images|graphics)/
-        unless env["REQUEST_URI"] =~ excluded_path_prefixes or path =~ excluded_path_prefixes
-          if route = Porthos::Routing::Resolver.new(path)
-            path.replace(route.path) if route.path
-            filter_parameters.merge!(route.params)
-          end
-
+      if !(env["REQUEST_URI"] =~ excluded_path_prefixes) || !(path =~ excluded_path_prefixes)
+        if route = Porthos::Routing::Resolver.new(path)
+          path.replace(route.path) if route.path
+          filter_parameters.merge!(route.params)
           matches = find_routes_without_filtering(env).map do |match, parameters, route|
             [ match, parameters.merge(filter_parameters), route ]
           end
         end
+      end
     end
 
     matches

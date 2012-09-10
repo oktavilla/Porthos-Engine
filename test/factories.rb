7a4d486806069ea1764d31827ca775d2125a2802
Joel Junström <joel.junstrom@oktavilla.se>
2012-09-10 12:02:39 +0200

    Use new factory girl dsl when defining attributes

--- test/factories.rb
+++ test/factories.rb
@@ -28,48 +28,48 @@ end
 
 FactoryGirl.define do
   factory :tag do |f|
-    f.name { FactoryGirl.generate(:tag_name) }
+    name { FactoryGirl.generate(:tag_name) }
   end
 
   factory :user do |f|
-    f.first_name 'Richie'
-    f.last_name 'Hawtin'
-    f.email { FactoryGirl.generate(:email) }
-    f.username { FactoryGirl.generate(:username) }
-    f.password 'password'
-    f.password_confirmation 'password'
+    first_name 'Richie'
+    last_name 'Hawtin'
+    email { FactoryGirl.generate(:email) }
+    username { FactoryGirl.generate(:username) }
+    password 'password'
+    password_confirmation 'password'
   end
 
   factory :node do |f|
-    f.url { FactoryGirl.generate(:url) }
-    f.status 1
-    f.controller "pages"
-    f.action "index"
+    url { FactoryGirl.generate(:url) }
+    status 1
+    controller "pages"
+    action "index"
   end
 
   factory :root_node, :parent => :node do |f|
-    f.url nil
-    f.name 'Start'
+    url nil
+    name 'Start'
   end
 
   factory :link_list do |f|
-    f.title { FactoryGirl.generate(:title) }
-    f.handle { FactoryGirl.generate(:handle) }
+    title { FactoryGirl.generate(:title) }
+    handle { FactoryGirl.generate(:handle) }
   end
 
   factory :link do |f|
-    f.title { FactoryGirl.generate(:title) }
-    f.url { FactoryGirl.generate(:url) }
+    title { FactoryGirl.generate(:title) }
+    url { FactoryGirl.generate(:url) }
   end
 
   factory :node_link do |f|
-    f.title { FactoryGirl.generate(:title) }
-    f.association :node, { factory: :node }
+    title { FactoryGirl.generate(:title) }
+    association :node, { factory: :node }
   end
 
   factory :content_template do |f|
-    f.label { FactoryGirl.generate(:title) }
-    f.datum_templates {
+    label { FactoryGirl.generate(:title) }
+    datum_templates {
       [
         FactoryGirl.build(:string_field_template),
         FactoryGirl.build(:text_field_template),
@@ -81,75 +81,75 @@ FactoryGirl.define do
   end
 
   factory :datum_template do |f|
-    f.label { FactoryGirl.generate(:title) }
-    f.handle { FactoryGirl.generate(:title) }
-    f.required false
+    label { FactoryGirl.generate(:title) }
+    handle { FactoryGirl.generate(:title) }
+    required false
   end
 
   factory :field_template, :class => FieldTemplate, :parent => :datum_template do |f|
   end
 
   factory :string_field_template, :class => StringFieldTemplate, :parent => :field_template do |f|
-    f.input_type 'string'
+    input_type 'string'
   end
 
   factory :text_field_template, :parent => :string_field_template do |f|
-    f.multiline true
+    multiline true
   end
 
   factory :rich_text_field_template, :parent => :text_field_template do |f|
-    f.allow_rich_text true
+    allow_rich_text true
   end
 
   factory :boolean_field_template, :parent => :field_template do |f|
-    f.input_type 'boolean'
+    input_type 'boolean'
   end
 
   factory :date_field_template, :parent => :field_template do |f|
-    f.input_type 'date'
+    input_type 'date'
   end
 
   factory :datum_collection_template, :class => DatumCollectionTemplate, :parent => :datum_template do |f|
   end
 
   factory :field_set_template do |f|
-    f.label { FactoryGirl.generate(:title) }
-    f.handle { FactoryGirl.generate(:handle) }
-    f.content_template { FactoryGirl.build(:content_template) }
+    label { FactoryGirl.generate(:title) }
+    handle { FactoryGirl.generate(:handle) }
+    content_template { FactoryGirl.build(:content_template) }
   end
 
   factory :datum do |f|
-    f.label { FactoryGirl.generate(:title) }
-    f.handle { FactoryGirl.generate(:handle) }
+    label { FactoryGirl.generate(:title) }
+    handle { FactoryGirl.generate(:handle) }
   end
 
   factory :field, :class => Field, :parent => :datum do |f|
   end
 
   factory :string_field, :class => StringField, :parent => :field do |f|
-    f.input_type 'string'
+    input_type 'string'
   end
 
   factory :text_field, :parent => :string_field do |f|
-    f.multiline true
+    multiline true
   end
 
   factory :rich_text_field, :parent => :text_field do |f|
-    f.allow_rich_text true
+    allow_rich_text true
   end
 
   factory :boolean_field, :parent => :field do |f|
-    f.input_type 'boolean'
+    input_type 'boolean'
   end
 
   factory :date_field, :parent => :field do |f|
-    f.input_type 'date'
+    input_type 'date'
   end
 
   factory :field_set do |f|
-    f.label { FactoryGirl.generate(:title) }
-    f.handle { FactoryGirl.generate(:title) }
-    f.data {
+    label { FactoryGirl.generate(:title) }
+    handle { FactoryGirl.generate(:title) }
+    data {
       [
         FactoryGirl.build(:string_field),
         Field.from_template(FactoryGirl.build(:text_field_template)),
@@ -164,9 +164,9 @@ FactoryGirl.define do
   end
 
   factory :page_template do |f|
-    f.label { FactoryGirl.generate(:title) }
-    f.handle { FactoryGirl.generate(:handle) }
-    f.datum_templates {
+    label { FactoryGirl.generate(:title) }
+    handle { FactoryGirl.generate(:handle) }
+    datum_templates {
       [
         FactoryGirl.build(:string_field_template, :label => 'Tagline'),
         FactoryGirl.build(:string_field_template),
@@ -177,8 +177,8 @@ FactoryGirl.define do
   end
 
   factory :hero_page_template, :parent => :page_template, :class => PageTemplate do |f|
-    f.page_label "Name"
-    f.datum_templates {
+    page_label "Name"
+    datum_templates {
       [
         FactoryGirl.build(:string_field_template, :label => 'Tagline', :handle => 'tagline', :required => true),
         FactoryGirl.build(:text_field_template, :label => 'Description', :handle => 'description', :required => true),
@@ -191,18 +191,18 @@ FactoryGirl.define do
   end
 
   factory :item do |f|
-    f.title { FactoryGirl.generate(:title) }
+    title { FactoryGirl.generate(:title) }
   end
 
   factory :section, :parent => :item, :class => Section do |f|
   end
 
   factory :page, :parent => :item, :class => Page do |f|
-    f.published_on { 1.week.ago }
+    published_on { 1.week.ago }
   end
 
   factory :asset do |f|
-    f.association :created_by, :factory => :user
+    association :created_by, :factory => :user
   end
 
   factory :image_asset, :parent => :asset, :class => ImageAsset do |f|
@@ -213,6 +213,6 @@ FactoryGirl.define do
   end
 
   factory :author do |f|
-    f.handle { FactoryGirl.generate(:handle) }
+    handle { FactoryGirl.generate(:handle) }
   end
 end
